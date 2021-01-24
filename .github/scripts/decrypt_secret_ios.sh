4a3ec4abf7d34a45f995838ea00f27001d1ab913
Dan Gilbert <ddg@daentech.co.uk>
2021-01-24 13:17:20 +0100

    Make android build run on linux. Fix iOS google play plist location

--- .github/scripts/decrypt_secret_ios.sh
+++ .github/scripts/decrypt_secret_ios.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_GOOGLE_PLIST_ENCRYPTION_SECRET" \
---output $HOME/secrets/GoogleService-Info.plist ios/Runner/GoogleService-Info.plist.gpg
+--output $HOME/secrets/GoogleService-Info.plist ios/GoogleService-Info.plist.gpg
