e21e0132739e4c1744c6e28a7fac1cbaf566e595
Dan Gilbert <ddg@daentech.co.uk>
2021-02-09 16:54:51 +0100

    Update saved workflows...

--- .github/scripts/decrypt_secret_android.sh
+++ .github/scripts/decrypt_secret_android.sh
@@ -3,6 +3,10 @@
 # Decrypt the file
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_DEPLOY_KEY_SECRET_PASSPHRASE" \
+--output $HOME/secrets/key.jks android/app/key.jks.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$KEY_PROPERTIES_SECRET_PASSPHRASE" \
+--output $HOME/secrets/key.properties android/key.properties.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_PLAY_SERVICE_ACCOUNT_SECRET_PASSPHRASE" \
 --output $HOME/secrets/google-play-service.json android/google-play-service.json.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_GOOGLE_JSON_ENCRYPTION_SECRET" \
