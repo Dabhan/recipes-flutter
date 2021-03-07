8d1a5910a5e0764d55973eab40d8ed49936a7560
Dan Gilbert <ddg@daentech.co.uk>
2021-03-07 14:36:41 +0100

    Fix firebase initialisation and ios dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:recipes/app.dart';
 import 'package:recipes/state_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(new StateWidget(
-  child: new RecipesApp(),
- ));
-    
+import 'ui/screens/loading.dart';
+import 'ui/screens/somethingwentwrong.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(new StateWidget(
+    child: FutureBuilder(
+      // Initialize FlutterFire
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return MaterialApp(home: SomethingWentWrong());
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return RecipesApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return MaterialApp(home: Loading());
+      },
+    ),
+  ));
+}
---
+++ lib/ui/screens/somethingwentwrong.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SomethingWentWrong extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    BoxDecoration _buildBackground() {
+      return BoxDecoration(
+        image: DecorationImage(
+          image:
+              AssetImage("assets/images/brooke-lark-wMzx2nBdeng-unsplash.jpg"),
+          fit: BoxFit.cover,
+        ),
+      );
+    }
+
+    Text _buildText() {
+      return Text(
+        'Recipes',
+        style: Theme.of(context).textTheme.headline,
+        textAlign: TextAlign.center,
+      );
+    }
+
+    return Scaffold(
+      // We do not use backgroundColor property anymore.
+      // New Container widget wraps our Center widget:
+      body: Container(
+        decoration: _buildBackground(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildText(),
+              SizedBox(height: 50.0),
+              Text("Something went wrong. Check your connection and try again"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
